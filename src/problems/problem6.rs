9dd604a1d2f314f50a76d19d226db121cb62b48d
m-shepherd <michael.shepherd2@sky.uk>
2023-12-06 21:29:41 +0000

    Solved problem 6 part 2

--- src/problems/problem6.rs
+++ src/problems/problem6.rs
@@ -8,20 +8,31 @@ impl Problem for Problem6 {
         let data = fileutils::load_problem_file(6);
 
         let lines: Vec<&str> = data.split("\n").map(|x| x.trim()).collect();
-        let time_elements: Vec<u32> = lines[0].split(" ").map(|x| x.trim()).filter(|x| x.len() > 0 && x != &"Time:").map(|x| x.parse::<u32>().unwrap()).collect();
-        let distance_elements: Vec<u32> = lines[1].split(" ").map(|x| x.trim()).filter(|x| x.len() > 0 && x != &"Distance:").map(|x| x.parse::<u32>().unwrap()).collect();
+        let time_elements: Vec<&str> = lines[0].split(" ").map(|x| x.trim()).filter(|x| x.len() > 0 && x != &"Time:").collect();
+        let distance_elements: Vec<&str> = lines[1].split(" ").map(|x| x.trim()).filter(|x| x.len() > 0 && x != &"Distance:").collect();
 
-        let mut result = 1;
-        for i in 0..time_elements.len() {
-            let mut num_options = 0;
-            for j in 1..time_elements[i] {
-                let distance_travelled = j * (time_elements[i] - j); 
-                if distance_travelled > distance_elements[i] {
-                    num_options += 1;
-                }
-            }
-            result *= num_options;
+        let mut time_string = String::from("");
+        for element in time_elements {
+            time_string += element; 
         }
-        println!("Result is {result}");
+        println!("Time {time_string}");
+        let time = time_string.parse::<u64>().unwrap();
+
+        let mut distance_string = String::from("");
+        for element in distance_elements {
+            distance_string += element;
+        }
+        println!("Distance {distance_string}");
+        let distance = distance_string.parse::<u64>().unwrap();
+
+        let mut num_options = 0;
+        for j in 1..time {
+            let distance_travelled = j * (time - j); 
+            if distance_travelled > distance {
+                num_options += 1;
+            }
+        }
+        println!("Result is {num_options}");
     }
 }
+
